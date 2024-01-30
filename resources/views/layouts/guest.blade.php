40748deb9351c87b3139a8418a763965cd647149
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-01-30 08:15:16 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -108,7 +108,7 @@
 			<div class="modal-dialog modal-lg">
 				<div class="modal-content">
 					<div class="modal-header border-bottom-0">
-						<h5 class="modal-title text-center">Contactez-nous ou rendez-vous à notre siège social</h5>
+						<h4 class="modal-title text-center">Contactez-nous ou rendez-vous à notre siège social</h4>
 						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
 
@@ -117,7 +117,7 @@
 							<!-- Container -->
 							<div class="container">
 								<div class="row">
-									<div class="col-12 text-center">
+									<div class="col-12">
 										<div class="widget-text margin-bottom-30">
 											<p class="lead d-flex justify-between">
 												<span class="ti-location-pin me-1 fs-5 theme-color"></span>
@@ -132,12 +132,10 @@
 												</span>
 											</p>
 										</div>
-		
-										<!-- Map -->
-										<div class="map p-sm-4">
+
+										<div class="map">
 											<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31827.877579898413!2d15.262481110839843!3d-4.319647500000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33e2ff4df57f%3A0xd38901f99027183b!2sGael%20Ministries!5e0!3m2!1sfr!2scd!4v1700871404177!5m2!1sfr!2scd" class="w-100" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 										</div>
-										<!-- Map -->
 									</div>
 								</div>
 							</div>
