a52a77a742619a17de8a9955b7f5ef286867dfb3
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-01-30 07:48:25 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -105,8 +105,8 @@
     <body data-res-from="1025">
 		<!--Modal-->
 		<div id="donateModal" class="modal fade" tabindex="-1">
-			<div class="modal-dialog">
-				<div class="modal-content modal-lg">
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
 					<div class="modal-header">
 						{{-- <h5 class="modal-title">Modal title</h5> --}}
 						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
